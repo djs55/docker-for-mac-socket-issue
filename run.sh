28e44a06c1b2e948b9f5c3e1307999416d7cda9d
David Scott <dave@recoil.org>
2018-06-07 15:33:37 +0100

    Fail if more than 25 fds are consumed in 5s
    Signed-off-by: David Scott <dave@recoil.org>

--- run.sh
+++ run.sh
@@ -8,10 +8,21 @@ finish(){
 }
 trap finish EXIT
 
+initial=$(lsof -i -P -n | grep com.dock | wc -l | sed 's/ //g')
+
 echo "Starting local HTTP server on port 8000"
 env PORT=8000 node app.js &
 HTTPPID=$!
 echo "Starting containers..."
 docker-compose up -d --build
 
-watch "lsof -i -P -n | grep com.dock | wc -l"
+# the sockets leak about 10 per second
+sleep 5
+
+final=$(lsof -i -P -n | grep com.dock | wc -l | sed 's/ //g')
+count=$(expr "${final}" - "${initial}")
+if [ "${count}" -gt 25 ]; then
+  echo "During the test we consumed ${count} fds which looks like a leak"
+  exit 1
+fi
+exit 0
