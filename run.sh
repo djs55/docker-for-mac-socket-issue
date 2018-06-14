656cf5fe9c4cb50d4ad46aee0450aee9e666db32
David Scott <dave@recoil.org>
2018-06-14 12:27:11 +0100

    Move the node server into a container
    Signed-off-by: David Scott <dave@recoil.org>

--- run.sh
+++ run.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -e
 
-HTTPPID=""
+server="node-server"
 finish(){
   echo "Exiting."
-  kill $HTTPPID || echo HTTP server has already been shutdown
+  docker kill "${server}" || echo server has already shutdown
   docker-compose down
 }
 trap finish EXIT
 
 initial=$(lsof -i -P -n | grep com.dock | wc -l | sed 's/ //g')
 
+docker pull node:alpine
+
 echo "Starting local HTTP server on port 8000"
-env PORT=8000 node app.js &
-HTTPPID=$!
+docker run --name "${server}" --rm -d -v $(pwd):/code -e PORT=8000 -p 8000:8000 node:alpine node /code/app.js
+
 echo "Starting containers..."
 docker-compose up -d --build
 
@@ -21,6 +23,7 @@ sleep 5
 
 final=$(lsof -i -P -n | grep com.dock | wc -l | sed 's/ //g')
 count=$(expr "${final}" - "${initial}")
+echo "At the beginning we consumed ${initial} fds. At the end we consumed ${final}."
 if [ "${count}" -gt 25 ]; then
   echo "During the test we consumed ${count} fds which looks like a leak"
   exit 1
